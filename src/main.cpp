005d53ce4bc26e547b8fd05800063c1feeb921c9
Jose Maia <maianeto2014@gmail.com>
2023-04-29 16:18:04 -0300

    Project is now complete

--- src/main.cpp
+++ src/main.cpp
@@ -190,7 +190,6 @@ void printMenu(List<Musica>& L, List<Playlist>& P){
     while((choice != 0) or (quit != 0));
 }
 
-// TODO Implement removeFromPlaylist function
 int main(){
     int index;
     string titulo, artista;
@@ -200,126 +199,6 @@ int main(){
     Playlist p1, p2, p3;
 
     printMenu(l, playlists);
-
-    // cadastrarPlaylist(playlists);
-    // cadastrarPlaylist(playlists);
-
-    //cout << buscarPlaylists(playlists)->data.nome;
-
-
-
-    //cout << playlists.size;
-
-
-    //cout << playlists.search(1)->data.nome << endl;
-    //cout << playlists.search(2)->data.nome << endl;
-    //cout << playlists.search(3)->data.nome << endl;
-
-
-/*
-    cadastrarPlaylist(playlists);
-    cadastrarPlaylist(playlists);
-    cadastrarPlaylist(playlists);
-
-    cout << playlists.search(1)->data.nome << endl;
-    // cout << playlists.search(1)->data.musicas.search(1)->data << endl;
-    // cout << playlists.search(1)->data.musicas.search(2)->data << endl;
-
-    cout << playlists.search(2)->data.nome << endl;
-    // cout << playlists.search(2)->data.musicas.search(1)->data << endl;
-    // cout << playlists.search(2)->data.musicas.search(2)->data << endl;
-
-    cout << playlists.search(3)->data.nome << endl;
-
-*/
-
-
-    //cout << "Testando" << endl;
-    //cout << "List<Musica> l: " << endl;
-    //l.printList();
-
-
-
-
-/*
-    cout << "Playlist Name:" << endl;
-    getline(cin, p1.nome);
-    for(int i = 0; i < 2; i++){
-        getline(cin, titulo);
-        getline(cin, artista);
-
-        m1 = Musica(titulo, artista); // Create a new song;
-        p1.musicas.insertNode(m1);
-    }
-
-    cout << "Playlist Name:" << endl;
-    getline(cin, p2.nome);
-    for(int i = 0; i < 2; i++){
-        getline(cin, titulo);
-        getline(cin, artista);
-
-        m1 = Musica(titulo, artista); // Create a new song;
-        p2.musicas.insertNode(m1);
-    }
-
-    cout << "Playlist Name:" << endl;
-    getline(cin, p3.nome);
-    for(int i = 0; i < 2; i++){
-        getline(cin, titulo);
-        getline(cin, artista);
-
-        m1 = Musica(titulo, artista); // Create a new song;
-        p3.musicas.insertNode(m1);
-    }
-
-    cout << endl << "_________________________________" << endl;
-    cout << "Playlists" << endl;
-
-    cout << p1.nome << endl;
-    p1.musicas.printList();
-
-    cout << "_________________________________" << endl;
-
-    cout << p2.nome << endl;
-    p2.musicas.printList();
-
-    cout << "_________________________________" << endl;
-
-    cout << p3.nome << endl;
-    p3.musicas.printList();
-
-
-    cout << "Search a song by the index:" << endl;
-    cin >> index;
-    cout << p2.musicas.search(index)->data;
-
-    l.insertNode(m1);
-    l.printList();
-
-    List<Playlist> plays;
-    plays.insertNode(p1);
-    plays.insertNode(p2);
-    plays.insertNode(p3);
-    cout << plays.search(1)->data.nome << endl;
-    cout << plays.search(1)->data.musicas.search(1)->data << endl;
-    cout << plays.search(1)->data.musicas.search(2)->data << endl;
-
-    cout << plays.search(2)->data.nome << endl;
-    cout << plays.search(2)->data.musicas.search(1)->data << endl;
-    cout << plays.search(2)->data.musicas.search(2)->data << endl;
-
-    cout << plays.search(3)->data.nome << endl;
-    cout << plays.search(3)->data.musicas.search(1)->data << endl;
-    cout << plays.search(3)->data.musicas.search(2)->data << endl;
-    
-    cout << "_________________________________" << endl;
-
-    plays.printList();
-
-    cout << "_________________________________" << endl;
-
-
-*/
 
     return 0;
 }
